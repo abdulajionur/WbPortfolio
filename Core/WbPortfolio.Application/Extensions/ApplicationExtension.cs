24f447e7dc0d062224c5e78632bf962cebb100de
onurabdulaji <onurabdulaji@gmail.com>
2025-03-20 02:43:35 +0100

    Mapster eklendi ve BaseDTO olusturuldu , FluentValidation eklendi ve Behaviour olustuldu.
    Servisler external ve internal olarak ikiyi ayrildi , bunun sebebi internal CRUD ve external infastructure ile calisacak olmasi ornek olarak mail servisi

--- Core/WbPortfolio.Application/Extensions/ApplicationExtension.cs
+++ Core/WbPortfolio.Application/Extensions/ApplicationExtension.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Mapster;
+using MapsterMapper;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using WbPortfolio.Application.Validators;
 
 namespace WbPortfolio.Application.Extensions;
 
@@ -6,5 +12,14 @@ public static class ApplicationExtension
 {
     public static void AddApplicationLayer(this IServiceCollection services)
     {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        
+        var config = TypeAdapterConfig.GlobalSettings;
+        config.Scan(Assembly.GetExecutingAssembly());
+        services.AddSingleton(config);
+        services.AddScoped<IMapper, ServiceMapper>();
+
+        services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
     }
 }
---
+++ Core/WbPortfolio.Application/Mapster/Map/BaseDto.cs
@@ -0,0 +1,11 @@
+﻿using Mapster;
+
+namespace WbPortfolio.Application.Mapster.Map;
+
+public abstract class BaseDto<TDto, TEntity> : IRegister where TDto : class, new() where TEntity : class, new()
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<TDto, TEntity>().TwoWays();
+    }
+}
