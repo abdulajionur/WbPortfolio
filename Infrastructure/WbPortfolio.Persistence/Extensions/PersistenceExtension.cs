f0bffce65099a238de700a8077848c0f7b5fd05c
onurabdulaji <onurabdulaji@gmail.com>
2025-03-20 02:00:40 +0100

    Persistence Extension Eklendi

---
+++ Infrastructure/WbPortfolio.Persistence/Extensions/PersistenceExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WbPortfolio.Persistence.Context.Data;
+
+namespace WbPortfolio.Persistence.Extensions;
+
+public static class PersistenceExtension
+{
+    public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+    }
+}
