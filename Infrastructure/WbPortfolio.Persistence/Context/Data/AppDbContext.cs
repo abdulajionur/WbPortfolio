ec2c8f83c69f50d1e43c58b93a820beb764f7e6f
onurabdulaji <onurabdulaji@gmail.com>
2025-03-20 01:46:11 +0100

    DbContext Eklendi

---
+++ Infrastructure/WbPortfolio.Persistence/Context/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WbPortfolio.Domain.Entities;
+
+namespace WbPortfolio.Persistence.Context.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options) {}
+    public AppDbContext()  { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+    public DbSet<About> Abouts { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Education> Educations { get; set; }
+    public DbSet<Form> Forms { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<Resume> Resumes { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<Skill> Skills { get; set; }
+    public DbSet<SocialMedia> SocialMedias { get; set; }
+    public DbSet<Stat> Stats { get; set; }
+    public DbSet<Summary> Summaries { get; set; }
+
+}
